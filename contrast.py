1f918c2cd6bd35cf31c4634472110afb3b63ae1e
Zoixxz <zoixxz@gmail.com>
2018-10-26 13:50:33 +0200

    update with current measurement data

--- contrast.py
+++ contrast.py
@@ -35,21 +35,31 @@ mV_max_four = np.array([-135., -205., -275., -326., -322., -330., -336., -330., 
 mV_min_four = np.array([-386., -504., -628., -731., -740., -763., -772., -760., -742., -713., -686., -643., -540., -409., -286.])
 
 
+#measurement 5 - mean (measurement 26.10.2018)
+angle_eraser_five = np.array([0, 10, 20, 30, 35, 40, 45, 50, 55, 60, 65, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180])
+mV_max_five = np.array([-59.03, -70.2, -82, -86.1, -89.4, -87.8, -86.3, -89.9, -77.35, -71.6, -67.32, -60.8, -40.4, -25.8,-12.09, -8.3, -2.3, 4.6, 2.9, -11, -21, -33.24, -44.5])
+mV_min_five = np.array([-114, -147.7, -175.2, -193.6, -200, -202.8, -201.9, -198.1, -188.8, -179.8, - 170.3, -158, -126.9,-92.7,-58.35, -32.3, -14.6, -6.37, -9.47, -23.52, -44, -74.26, -104.5])
+error = 5
+
+mV_max_five -= 5
+mV_min_five -= 5
+
 x_one = np.linspace(82, 172, 50)
 x_two = np.linspace(90, 180, 50)
 x_three = np.linspace(45, 135, 50)
 x_four = np.linspace(0, 180, 50)
+x_five = np.linspace(0, 100, 50)
 
 def contrast(V_max, V_min):
 	return (V_max - V_min)/(V_max + V_min)
 
-print(contrast(mV_max_one, mV_min_one))
-print(mV_min_one.shape, mV_max_one.shape)
-plt.plot(angle_eraser_four, contrast(mV_max_four, mV_min_four), 'b+')
-plt.plot(x_four, abs(np.cos(2*x_four /180 * math.pi - math.pi/4)), 'r')
-plt.plot(angle_eraser_one, contrast(mV_max_one, mV_min_one), 'g+')
-plt.plot(angle_eraser_two, contrast(mV_max_two, mV_min_two))
+
+print(contrast(mV_max_five, mV_min_five))
+plt.plot(angle_eraser_five, contrast(mV_max_five, mV_min_five), 'b+')
+plt.plot(x_five, abs(np.cos(2*x_five /180 * math.pi - math.pi/4)), 'r')
 plt.xlabel('angle of the eraser [°]')
 plt.ylabel('contrast [1]')
 plt.show()
+
+
 
